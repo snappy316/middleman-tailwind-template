25ad03a9a3a925c8ae114688da634f55f682fc01
David Ker <snappy316@gmail.com>
2020-07-17 11:20:32 -0700

    Add Tailwind UI configuration

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  variants: {},
-  plugins: []
+  plugins: [
+    require('@tailwindcss/ui'),
+  ]
 }
